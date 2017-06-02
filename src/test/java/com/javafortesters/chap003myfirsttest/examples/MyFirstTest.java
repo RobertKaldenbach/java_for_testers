048ccd64cd8824a12a05e00fc2a01e291d51fcd5
RobertKaldenbach <robert.kaldenbach@gmail.com>
2017-06-01 23:20:17 -0400

    ini Java for Testers Repo

---
+++ src/test/java/com/javafortesters/chap003myfirsttest/examples/MyFirstTest.java
@@ -0,0 +1,40 @@
+/**
+ * Created by bobert on 6/1/17.
+ */
+
+
+
+package com.javafortesters.chap003myfirsttest.examples;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class MyFirstTest {
+
+    @Test
+    public void canAddTwoPlusTwo(){
+
+        int answer = 2+2;
+        assertEquals("2+2=4",4,answer);
+
+    }
+
+    @Test
+    public void canSubtractTwoFromTwo(){
+        int answer = 2-2;
+        assertEquals("2-2=0",0,answer);
+    }
+
+    @Test
+    public void canDivideFourByTwo(){
+        int answer = 4/2;
+        assertEquals("4/2=2",2,answer);
+    }
+
+    @Test
+    public void canMultiplyTwoByTwo(){
+        int answer = 2*2;
+        assertEquals("2*2=4",4,answer);
+    }
+}
