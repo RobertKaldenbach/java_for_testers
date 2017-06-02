162abc742535fafcd635ee67215d290fcb613f7d
RobertKaldenbach <robert.kaldenbach@gmail.com>
2017-06-02 07:52:40 -0400

    added commits for MyFirstTest class

--- src/test/java/com/javafortesters/chap003myfirsttest/examples/MyFirstTest.java
+++ src/test/java/com/javafortesters/chap003myfirsttest/examples/MyFirstTest.java
@@ -12,6 +12,7 @@ import static org.junit.Assert.assertEquals;
 
 public class MyFirstTest {
 
+    //Test for 2+2=4
     @Test
     public void canAddTwoPlusTwo(){
 
@@ -19,19 +20,19 @@ public class MyFirstTest {
         assertEquals("2+2=4",4,answer);
 
     }
-
+    //Test for 2-2=0
     @Test
     public void canSubtractTwoFromTwo(){
         int answer = 2-2;
         assertEquals("2-2=0",0,answer);
     }
-
+    //Test for 4/2=2
     @Test
     public void canDivideFourByTwo(){
         int answer = 4/2;
         assertEquals("4/2=2",2,answer);
     }
-
+    //Test for 2*2=4
     @Test
     public void canMultiplyTwoByTwo(){
         int answer = 2*2;
